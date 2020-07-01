d05710b096aa5d1416017242ab6668152d8efc2c
YumizSui <buggymercury@gmail.com>
2020-07-01 12:47:54 +0900

    shuusei

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -17,19 +17,17 @@
       <th width="100">完了</th>
       <th width="100">削除</th>
       <tr v-for="(task, ind) in tasks" :key="task.id">
-        <td v-if="task.status == false">
-          まだ
-        </td>
+        <td v-if="task.status == false">まだ</td>
         <td v-else>ヨシ</td>
+        <td v>{{ task.content }}</td>
         <td v-if="task.deadline == ''">締め切なし</td>
-        <td v-else>{{ task.deadline.toLocaleString() }}</td>
+        <td v-else>{{ task.deadline }}</td>
         <td>
           <button @click="task.status = true">完了</button>
         </td>
         <td>
           <button @click="deleteTask(ind)">削除</button>
         </td>
-        <td>{{ ind }}</td>
       </tr>
     </table>
   </div>
@@ -43,19 +41,31 @@ export default {
       tasks: [
         {
           id: 0,
-          content: "todoをやる",
+          content: "todoをつくる",
           deadline: "",
-          status: false,
+          status: false
         },
         {
           id: 1,
-          content: "課題をやれ",
-          deadline: "2020/5/5",
-          status: true,
+          content: "人工知能課題",
+          deadline: "2020/7/14",
+          status: false
         },
+        {
+          id: 2,
+          content: "離散構造",
+          deadline: "2020/7/27",
+          status: false
+        },
+        {
+          id: 3,
+          content: "コンパイラ構成",
+          deadline: "2020/7/17",
+          status: false
+        }
       ],
       newTask: "",
-      newDeadLine: "",
+      newDeadLine: ""
     };
   },
   methods: {
@@ -64,13 +74,13 @@ export default {
         id: id,
         content: this.newTask,
         deadline: this.newDeadLine,
-        status: false,
+        status: false
       });
     },
     deleteTask(id) {
       this.tasks.splice(id, 1);
-    },
-  },
+    }
+  }
 };
 </script>
 
